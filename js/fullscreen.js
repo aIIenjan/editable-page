68f378b516bf2e1cb690cf907e15e3475f2e4b56
jiangjinhua <13922779059@139.com>
2017-11-21 19:25:25 +0800

    修改背景颜色

--- js/fullscreen.js
+++ js/fullscreen.js
@@ -1,43 +1,45 @@
 (function () {
+    var $obj = $('#view-fullscreen');
     var viewFullScreen = document.getElementById("view-fullscreen");
     if (viewFullScreen) {
-        viewFullScreen.addEventListener("click", function () {
-            var docElm = document.documentElement;
-            if (docElm.requestFullscreen) {
-                docElm.requestFullscreen();
+        var checkFull = viewFullScreen.addEventListener("click", function () {
+            if($obj.hasClass('on')) {
+                $obj.removeClass('on');
+                // $obj.find('span').html('全屏');
+                if (document.exitFullscreen) {
+                    document.exitFullscreen();
+                }
+                else if (document.msExitFullscreen) {
+                    document.msExitFullscreen();
+                }
+                else if (document.mozCancelFullScreen) {
+                    document.mozCancelFullScreen();
+                }
+                else if (document.webkitCancelFullScreen) {
+                    document.webkitCancelFullScreen();
+                }
+                // viewFullScreen.style.display = 'inline-block';
+                // cancelFullScreen.style.display = 'none';
+            }else {
+                $obj.addClass('on');
+                // $obj.find('span').html('退出全屏');
+                var docElm = document.documentElement;
+                if (docElm.requestFullscreen) {
+                    docElm.requestFullscreen();
+                }
+                else if (docElm.msRequestFullscreen) {
+                    docElm = document.body; //overwrite the element (for IE)
+                    docElm.msRequestFullscreen();
+                }
+                else if (docElm.mozRequestFullScreen) {
+                    docElm.mozRequestFullScreen();
+                }
+                else if (docElm.webkitRequestFullScreen) {
+                    docElm.webkitRequestFullScreen();
+                }
+                // cancelFullScreen.style.display = 'inline-block';
+                // viewFullScreen.style.display = 'none';
             }
-            else if (docElm.msRequestFullscreen) {
-                docElm = document.body; //overwrite the element (for IE)
-                docElm.msRequestFullscreen();
-            }
-            else if (docElm.mozRequestFullScreen) {
-                docElm.mozRequestFullScreen();
-            }
-            else if (docElm.webkitRequestFullScreen) {
-                docElm.webkitRequestFullScreen();
-            }
-            cancelFullScreen.style.display = 'inline-block';
-            viewFullScreen.style.display = 'none';
-        }, false);
-    }
-
-    var cancelFullScreen = document.getElementById("cancel-fullscreen");
-    if (cancelFullScreen) {
-        cancelFullScreen.addEventListener("click", function () {
-            if (document.exitFullscreen) {
-                document.exitFullscreen();
-            }
-            else if (document.msExitFullscreen) {
-                document.msExitFullscreen();
-            }
-            else if (document.mozCancelFullScreen) {
-                document.mozCancelFullScreen();
-            }
-            else if (document.webkitCancelFullScreen) {
-                document.webkitCancelFullScreen();
-            }
-            viewFullScreen.style.display = 'inline-block';
-            cancelFullScreen.style.display = 'none';
         }, false);
     }
 })();
