96772b84f8a4415793ba076d8c44b4de23343e64
jiangjinhua <13922779059@139.com>
2017-11-21 16:56:59 +0800

    添加版本

---
+++ js/common.js
@@ -0,0 +1,30 @@
+$(function() {
+	// 左边变化js
+	function autoHeight($obj) {
+		var $winHeight = $(window).height();
+		$obj.height($winHeight-90);
+	}
+	autoHeight($('.container'));
+
+	// 改变浏览器大小
+	$(window).resize(function() {
+		autoHeight($('.container'))
+	});
+
+	// 左浮动滚动条效果
+    $('.leftbar').mCustomScrollbar({
+		scrollButtons:{enable:true},
+		theme:"light-thick",
+		scrollbarPosition:"inside"
+	});
+
+	// 右边标尺
+    $('#content').ruler({
+        unit: 'px',
+        tickMajor: 100,
+        tickMinor: 20,
+        tickMicro: 10,
+        showLabel: true,
+        arrowStyle: 'arrow'
+    });
+})
