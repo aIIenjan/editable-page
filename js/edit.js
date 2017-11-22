e96a080b7ea1651a1b665ae123e419dd0d66b42a
jiangjinhua <13922779059@139.com>
2017-11-22 18:29:19 +0800

    修改js

--- js/edit.js
+++ js/edit.js
@@ -5,21 +5,12 @@ new Vue({
 		modelInfo: false,
 	},
 	methods:{
-<<<<<<< HEAD
 		modelInfoShow: function(){
-=======
-		modelInfoShow: function() {
->>>>>>> 9ba4c55168496416542508bbfd742225e710de9b
 			this.modelInfo = !this.modelInfo;
 			this.modelInfo ? $( '.model-info' ).css({left: 0}) : $( '.model-info' ).css({left: -502})
 		}
 	},
-<<<<<<< HEAD
 	mounted: function(){
-=======
-	mounted: function() {
->>>>>>> 9ba4c55168496416542508bbfd742225e710de9b
-
 	},
 	components:{
 
