a36366caeaa1cddac207fa93136f43ccf41c6456
ZhouYuTiana <1584424816@qq.com>
2017-11-22 11:48:37 +0800

    左边弹窗

--- js/edit.js
+++ js/edit.js
@@ -7,7 +7,7 @@ new Vue({
 	methods:{
 		modelInfoShow(){
 			this.modelInfo = !this.modelInfo;
-			console.log( this.modelInfo )
+			this.modelInfo ? $( '.model-info' ).css({left: 0}) : $( '.model-info' ).css({left: -502})
 		}
 	},
 	mounted(){
