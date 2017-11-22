254a4ba015319c408b8eee40ed5c3eadf2ea2587
ZhouYuTiana <1584424816@qq.com>
2017-11-22 11:07:35 +0800

    dialog

---
+++ js/edit.js
@@ -0,0 +1,26 @@
+
+new Vue({
+	el: '#app',
+	data: {
+		modelInfo: false,
+	},
+	methods:{
+		modelInfoShow(){
+			this.modelInfo = !this.modelInfo;
+			console.log( this.modelInfo )
+		}
+	},
+	mounted(){
+
+	},
+	components:{
+
+
+	}
+})
+
+
+
+
+
+
