e4a1c25c63134ef0b5ca163a558c9fec261cb474
ZhouYuTiana <1584424816@qq.com>
2017-11-23 17:39:01 +0800

    修改背景

--- js/edit.js
+++ js/edit.js
@@ -3,11 +3,21 @@ new Vue({
 	el: '#app',
 	data: {
 		modelInfo: false,
+		back_color: '',
+		isback_color: {background: ''},
 	},
 	methods:{
 		modelInfoShow: function(){
 			this.modelInfo = !this.modelInfo;
 			this.modelInfo ? $( '.model-info' ).css({left: 0}) : $( '.model-info' ).css({left: -502})
+		},
+		saveData: function(){
+			console.log( this.back_color )
+			if( this.back_color[0] =='#' ){
+				this.isback_color.background = this.back_color;
+			}else{
+				this.isback_color.background = 'rgb('+this.back_color+')';
+			}
 		}
 	},
 	mounted: function(){
